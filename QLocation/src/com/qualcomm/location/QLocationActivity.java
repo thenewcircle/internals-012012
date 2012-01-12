11bb51e931d146b4ca47c6d781df718905337fb3
Marko Gargenta <marko@marakana.com>
2012-01-12 15:17:30 -0800

    Fixed project properties.

--- QLocation/src/com/qualcomm/location/QLocationActivity.java
+++ QLocation/src/com/qualcomm/location/QLocationActivity.java
@@ -2,10 +2,12 @@ package com.qualcomm.location;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class QLocationActivity extends Activity {
+	static final String TAG = "QLocation";
 	TextView output;
 	
     /** Called when the activity is first created. */
@@ -18,6 +20,6 @@ public class QLocationActivity extends Activity {
     
     /** Called when button is clicked. */
     public void onClick(View v) {
-    		
+    		Log.d(TAG, "onClicked");
     }
 }
