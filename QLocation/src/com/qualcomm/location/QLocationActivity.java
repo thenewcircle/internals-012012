1c57e42755323d2be8fbf454698540f03a7bea3d
Marko Gargenta <marko@marakana.com>
2012-01-12 15:32:00 -0800

    Testing from Activity.

--- QLocation/src/com/qualcomm/location/QLocationActivity.java
+++ QLocation/src/com/qualcomm/location/QLocationActivity.java
@@ -1,6 +1,9 @@
 package com.qualcomm.location;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -21,5 +24,13 @@ public class QLocationActivity extends Activity {
     /** Called when button is clicked. */
     public void onClick(View v) {
     		Log.d(TAG, "onClicked");
+    		
+//    		List<String> providers = QLocationLib.getAllProviders();
+//    		for(String provider: providers) {
+//    			output.append("\n" + provider);
+//    		}
+    		
+    		Location location = QLocationLib.getLastKnownLocation("GPS2");
+    		output.append("\n"+location.toString());
     }
 }
