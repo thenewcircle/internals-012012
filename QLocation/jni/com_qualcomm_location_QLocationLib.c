3190fdd688c9dfdc05c4d0ea64fafcb202fa5e7b
Marko Gargenta <marko@marakana.com>
2012-01-12 17:09:52 -0800

    Added Common lib.

--- QLocation/jni/com_qualcomm_location_QLocationLib.c
+++ QLocation/jni/com_qualcomm_location_QLocationLib.c
@@ -15,7 +15,7 @@ JNIEXPORT jobjectArray JNICALL Java_com_qualcomm_location_QLocationLib__1getAllP
 
 JNIEXPORT jdoubleArray JNICALL Java_com_qualcomm_location_QLocationLib__1getLastKnownLocation
   (JNIEnv *env, jclass clazz, jstring provider) {
-	double data[] = {370000, 1280000};
+	double data[] = {37, 128};
 
 	jdoubleArray ret = (*env)->NewDoubleArray(env, 2);
 	(*env)->SetDoubleArrayRegion(env, ret, 0, 2, data);
---
+++ QLocationCommon/src/com/qualcomm/locationcommon/IQLocationService.aidl
@@ -0,0 +1,5 @@
+package com.qualcomm.locationcommon;
+
+interface IQLocationService {
+	List<String>	 getAllProviders();
+}
